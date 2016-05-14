20ceab7856f38aa687fc7632490ce89ae22c7be9
Gabriel Bronzatti Moro <tido4410@gmail.com>
2016-05-14 13:59:45 -0300

    Codes tiling and tiling with transpose are ready

--- src/MatricesMultiplication/tilingWithTranspose/MultiplicaMatrizes_seq.c
+++ src/MatricesMultiplication/tilingWithTranspose/MultiplicaMatrizes_seq.c
@@ -11,31 +11,18 @@
 #include <stdio.h>
 #include "cmp134.h"
 
-void transpose(double **B, int size)
-{
-	int i,j;
+#define HIDE_TIMER 0
 
-	for(i=0; i < size; i++)
-	{
-		for(j=0; j < size; j++)
-		{
-			B[i][j] = B[j][i];
-		}
-	}
-}
+int min(int, int);
 
-int min(int x, int y)
-{
-	if(x<y)
-		return x;
-	else
-		return y;
-}
+void transposeInplace(double **, int);
+
+void printerOfMatrix(double **, int);
 
 int main(int argc, char *argv[]) 
 {
 	/*Inicializacao de variaveis*/
-	int size=10,block=2;
+	int size=10,block;
 	double **A, **B, **R;
 
 	if (argc > 1){
@@ -43,7 +30,7 @@ int main(int argc, char *argv[])
 	}
 
 	/*Define aqui de acordo com tua máquina, 16, 32, 64 por ai, pra mim ficou massa com 64 pra 1000 elementos*/
-	block= 64;
+	block= 16;
 
 	/*Inicialização das matrizes*/
 	A = cmp134_create_matrix (size);
@@ -56,8 +43,8 @@ int main(int argc, char *argv[])
 	int i,j,jj,k,kk;
 	double tmp;
 
-	transpose(B, size);
-
+	transposeInplace(B, size);
+	
 	for(jj=0;jj < size; jj=jj+block)
 	{
 		for(kk=0; kk < size; kk=kk+block)
@@ -69,7 +56,7 @@ int main(int argc, char *argv[])
 					tmp=0;
 					for(k=kk; k < min(kk+block,size); k++)
 					{
-						tmp =+ A[i][k] * B[i][k];
+						tmp = tmp + A[i][k] * B[j][k];
 					}
 					R[i][j] = tmp;
 				}
@@ -78,12 +65,17 @@ int main(int argc, char *argv[])
 	}
 
 	CMP134_END_TIMER;
-	CMP134_REPORT_TIMER;
 
-	/*Temos que imprimir isso se não o compilador 
-	não faz a multiplicação por não se justificar essa operação*/
-	
-	printf("The last element is %.2f\n", R[size-1][size-1]);
+	if(HIDE_TIMER!=1)
+	{
+		CMP134_REPORT_TIMER;
+		/*Temos que imprimir isso se não o compilador 
+		não faz a multiplicação por não se justificar essa operação*/
+		//printf("The last element is %.2f\n", R[size*size-1]);
+	}else
+	{
+		printerOfMatrix(R,size);
+	}
 
 	cmp134_free_matrix(A,size);
 	cmp134_free_matrix(B,size);
@@ -91,4 +83,40 @@ int main(int argc, char *argv[])
 	
 	return 0;
 
+}
+
+void transposeInplace(double **B, int size)
+{
+	int i,j;
+	double tmp=0;
+	for(i = 0; i < size; i++)
+	{
+		for(j=i+1; j < size; j++)
+		{
+			tmp = B[i][j];
+			B[i][j] = B[j][i];
+			B[j][i] = tmp;
+		}
+	}
+	
+}
+
+int min(int x, int y)
+{
+	if(x<y)
+		return x;
+	else
+		return y;
+}
+
+void printerOfMatrix(double **R, int size)
+{
+	int i,j;
+
+	for(i=0; i < size; i++)
+	{
+		for(j=0; j < size; j++)
+			 printf("%f\t", R[i][j]);
+	    printf("\n");
+	}
 }
